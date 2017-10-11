a8b3246490a06dbc71ba98a2486abd378d157aa7
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-10-11 12:27:23 +0200

    Beginning

---
+++ README.md
@@ -0,0 +1,6 @@
+# aulefib - cache proxy server per il GAP
+
+Questo mini-progetto fornisce un server web che mostra la programmazione delle
+aule del Polo Fibonacci @ UniPi.
+
+https://fib.anto.pt
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+
+let cache = 'Updating cache...'
+let updateCache = () => {
+  request.post({
+    url: 'http://gap.adm.unipi.it/GAP-A-Fibonacci/newGAP-SI.cgi',
+    form: {query: 'guida_oggi'}
+    },
+    (err, res) => {
+      if (err) return console.error(err);
+      cache = res;
+      console.log('Cache update');
+    }
+  )
+}
+updateCache();
+setInterval(updateCache, 1000*60*30)
+
+app.get('/', function (req, res) {
+  res.send(cache.body);
+});
+
+app.listen(12007, function () {
+  console.log('Listening on port 12007')
+});
+
